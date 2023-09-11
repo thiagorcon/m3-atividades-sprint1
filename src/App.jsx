db9b90090b4a83bacf107639e59342483774cdd4
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-09-11 10:49:27 -0300

    cleaning base

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
 function App() {
-   const person = {
-      name: "Alex",
-      module: "M3",
-      age: 33,
-   };
-
-   const alertName = () => { 
-      alert(person.name);
-   }
-
    return (
       <>
-         <li>
-            <h3>
-               {person.name} - {person.module}
-            </h3>
-            <p>Tenho: {person.age} anos</p>
-            <button onClick={alertName}>Quem sou eu?</button>
-         </li>
+
       </>
    );
 }
