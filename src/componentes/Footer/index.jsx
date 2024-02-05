fe76489a57dbe8f4a59795eab6d5a8683bca5753
Thiago Rosa <thiagorcon@hotmail.com>
2024-02-05 10:40:50 -0300

    envio da parte 1 - criacao de componentes

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -1,9 +1,9 @@
 const Footer = () =>{
      
      return(
-        <>
+        <section>
         <p>Todos os direitos reservados - Thiago Rosa - 2024</p>
-        </>  
+        </section>  
      )
 }
 
